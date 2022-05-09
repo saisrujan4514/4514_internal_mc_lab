0f1193401f65ac204a5c3b1f14f9c4bdbdc9345a
mohan <mohan.saisrujan4@gmail.com>
2022-05-09 11:52:50 +0530

    commit_1

---
+++ app/src/main/java/com/example/a4514_internal_mc_lab/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4514_internal_mc_lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
